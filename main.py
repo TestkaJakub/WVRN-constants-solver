f42483069df58632382d37ce9098e14f48cab839
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-01-22 00:04:54 +0100

    priorities.json and README

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
+import json
 from WVRN import declaration
 from state_machine import FiniteStateMachine
 from bfs import *
 from data_processing import *
 
+# Load priorities from priorities.json
+def load_priorities(file_path):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+        return data.get("priorities", [])
+
 fsm = FiniteStateMachine()
 fsm = declaration(fsm)
 
-# Define your priority order
-PRIORITY = [
-            "addi 7",
-            "addi 6",
-            "addi 5",
-            "addi 4",
-            "addi 3",
-            "addi 2",
-            "addi 1",
-            "addi -8",
-            "addi -7",
-            "addi -6",
-            "addi -5",
-            "addi -4",
-            "addi -3",
-            "addi -2",
-            "addi -1",
-            "add acc",
-            "nand acc",
-            ]
+# Load priority order from JSON
+PRIORITY = load_priorities("priorities.json")
+
+# Validate if PRIORITY is loaded correctly
+if not PRIORITY:
+    raise ValueError("The priorities list is empty. Please check priorities.json.")
 
 shortest_paths_data = find_shortest_paths(fsm, "0", PRIORITY)  # Get shortest paths
 format_and_save_as_json(shortest_paths_data, "shortest_paths.json")  # Save as JSON
